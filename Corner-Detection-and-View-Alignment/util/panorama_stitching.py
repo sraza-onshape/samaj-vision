bad52fbdbe00595c001fe2fa02d6ddfdf2b1bb41
= <zainr7989@gmail.com>
2023-12-02 15:20:25 -0500

    improve stitching implementation

--- Corner-Detection-and-View-Alignment/util/panorama_stitching.py
+++ Corner-Detection-and-View-Alignment/util/panorama_stitching.py
@@ -8,42 +8,41 @@ class PanoramaStitcher:
     def composite_images(
         left_img,
         right_img,
-        affine_transform_matrix,
+        affine_transform_matrix_from_left_to_right,
         affine_transform_offset,
         plot_title: str = "",
-    ):
+    ) -> np.ndarray:
         """
         Horizontally align two overlapping images using an affine transform.
         """
-        # Determine output shape for the stitched panorama
-        output_shape = (
+        panorama_output_shape = (
             max(left_img.shape[0], right_img.shape[0]),
             left_img.shape[1] + right_img.shape[1],
         )
 
+        # Prepare for the affine transformation
+        affine_transform_matrix_from_right_to_left = np.linalg.inv(
+            affine_transform_matrix_from_left_to_right
+        ).T
+        affine_transform_offset_inv = affine_transform_offset
+        affine_transform_offset_inv[:2] *= -1
+
         # Apply the affine transformation to warp the left image onto the right
         left_img_transformed = ndimage.affine_transform(
             left_img,
-            affine_transform_matrix,
-            offset=np.squeeze(affine_transform_offset),
+            affine_transform_matrix_from_right_to_left,
+            offset=np.squeeze(affine_transform_offset_inv),
         )
-        stitched_image = np.zeros(output_shape)
-        stitched_image[
+
+        panorama_img = np.zeros(panorama_output_shape)
+        panorama_img[
             0 : left_img.shape[0], 0 : left_img.shape[1]
         ] = left_img_transformed
 
-        # Blend the right image onto the panorama
-        # right_img_transformed = ndimage.affine_transform(
-        #     right_img,
-        #     affine_transform_matrix,
-        #     offset=np.squeeze(affine_transform_offset),
-        # )
-        stitched_image[
-            : right_img.shape[0], left_img.shape[1] :
-        ] = right_img
+        panorama_img[: right_img.shape[0], left_img.shape[1] :] = right_img
 
         # Display the result
-        plt.imshow(stitched_image, cmap="gray")
+        plt.imshow(panorama_img, cmap="gray")
         plt.title(plot_title)
         plt.axis("off")  # for cleanliness, hide the axis lines and ticks
         plt.show()
